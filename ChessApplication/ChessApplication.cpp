c06e6f331dca5c688cc9dbd450610c710d3d5e0d
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2022-05-15 22:52:14 +0200

    Added images and chess board visual representation

--- ChessApplication/ChessApplication.cpp
+++ ChessApplication/ChessApplication.cpp
@@ -1,7 +1,28 @@
+#include <QLabel>
+#include <QHBoxLayout>
 #include "ChessApplication.h"
 
 ChessApplication::ChessApplication(QWidget *parent)
     : QMainWindow(parent)
 {
-    ui.setupUi(this);
+    // Create a label
+    QLabel* label = new QLabel(this);
+    QPixmap pixmap(":/ChessApplication/Images/ChessBoard.png");
+    label->setPixmap(pixmap);
+    label->setScaledContents(true);
+    label->setFixedWidth(600);
+    label->setFixedHeight(600);
+    label->setAlignment(Qt::AlignCenter);
+	
+    QWidget* widget = new QWidget;
+
+    // create horizontal layout
+    QHBoxLayout* layout = new QHBoxLayout;
+    // Add label to the the layout
+    layout->addWidget(label);
+
+    // set layout to widget
+    widget->setLayout(layout);
+
+    setCentralWidget(widget);
 }
