e0b9bb43940e531c1c127e03cea3587cd9d06079
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2022-05-17 00:43:52 +0200

    Testing layouts

--- ChessApplication/ChessApplication.cpp
+++ ChessApplication/ChessApplication.cpp
@@ -1,14 +1,30 @@
 #include <QLabel>
 #include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QScrollArea>
 #include "ChessApplication.h"
 #include "../ChessBackend/Board.h"
 
 ChessApplication::ChessApplication(QWidget *parent)
     : QMainWindow(parent)
 {
+    // Initiate the board
     Board::getInstance();
-	
-    // Create a label
+
+
+
+    // Create a grid
+	QVBoxLayout* gridVertical = new QVBoxLayout;
+
+	QLabel* whiteKing = new QLabel;
+    QPixmap whiteKingMap(":/Images/White King.png");
+    whiteKing->setPixmap(whiteKingMap);
+
+    gridVertical->addWidget(whiteKing);
+
+
+
+	// Create background label which contains the board
     QLabel* label = new QLabel(this);
     QPixmap pixmap(":/Images/ChessBoard.png");
     label->setPixmap(pixmap);
@@ -16,16 +32,15 @@ ChessApplication::ChessApplication(QWidget *parent)
     label->setFixedWidth(600);
     label->setFixedHeight(600);
     label->setAlignment(Qt::AlignCenter);
-	
-    QWidget* widget = new QWidget;
 
-    // create horizontal layout
-    QHBoxLayout* layout = new QHBoxLayout;
-    // Add label to the the layout
+    // Create horizontal layout
+    QHBoxLayout *layout = new QHBoxLayout();
+    layout->addLayout(gridVertical);
     layout->addWidget(label);
 
-    // set layout to widget
-    widget->setLayout(layout);
+    // Create widget
+    QWidget* mainWidget = new QWidget;
+    mainWidget->setLayout(layout);
 
-    setCentralWidget(widget);
+    setCentralWidget(mainWidget);
 }
